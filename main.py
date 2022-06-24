10d8c95bd9c17c5cce01c120106a311b6dd976f6
71smartie <straussmartenet@gmail.com>
2022-06-24 17:29:39 +1200

    Import password

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from secret import password
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     print_hi('Martenette')
     print('You are great, Sussie!')
     print('You are awesome!!!!')
-
+    print(password)
